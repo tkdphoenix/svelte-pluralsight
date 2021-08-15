4492fe09ccea1e4d2b4855e87c5820956f96b15d
Joel Grissom <joelgrissom5@gmail.com>
2021-08-14 20:55:43 -0700

    initial commit after svelte tutorial

---
+++ src/common/BackButtonRow.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Button from "../common/Button.svelte"
+</script>
+
+<nav>
+  <Button to="/">&lt; Back</Button>
+
+  <div class="text">
+    To the
+    <span>Library</span>
+
+  </div>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    align-items: center;
+    text-transform: uppercase;
+    margin-bottom: var(--spacingXLarge);
+  }
+  .text {
+    margin-left: var(--spacingSmall);
+  }
+  span {
+    font-weight: var(--typeWeightBold);
+  }
+</style>
